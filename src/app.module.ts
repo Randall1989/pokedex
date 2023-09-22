1d0766ed3f867ce8549b2b1bdec1a6f13b9831ae
rrodriguez <randall.rodriguez@gmail.com>
2023-09-22 17:24:18 -0600

    Commit Inicial

---
+++ src/app.module.ts
@@ -0,0 +1,37 @@
+import { Module } from '@nestjs/common';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { MongooseModule } from '@nestjs/mongoose';
+import { join } from 'path';
+import { PokemonModule } from './pokemon/pokemon.module';
+import { CommonModule } from './common/common.module';
+import { SeedModule } from './seed/seed.module';
+import { ConfigModule } from '@nestjs/config';
+import { EnvConfiguration } from './config/env.config';
+import { JoiValidationSchema } from './config/joi.validation';
+
+
+@Module({
+  imports: [ 
+    ConfigModule.forRoot({
+      load: [EnvConfiguration],
+      validationSchema: JoiValidationSchema,
+    }),
+
+    ServeStaticModule.forRoot({
+    rootPath: join(__dirname,'..','public'),
+    }),
+    MongooseModule.forRoot(process.env.MONGODB,{
+      dbName: 'pokemonsdb'
+    }),
+    PokemonModule,
+    CommonModule,
+    SeedModule
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {
+  constructor(){
+    console.log(process.env);
+  }
+}
